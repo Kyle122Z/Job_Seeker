b95e2bbc8e6436bf210c6a2b0604924a7f46f791
Kyle122Z <kyletrippk@gmail.com>
2022-07-10 13:35:44 +0300

    Creates the jobs model class

--- app/src/main/java/com/jobs/jobseeker/model/Jobs.java
+++ app/src/main/java/com/jobs/jobseeker/model/Jobs.java
@@ -1,8 +1,19 @@
 package com.jobs.jobseeker.model;
 
-public class jobs {
+public class Jobs {
 
-    String category, company_name, role, location, job_description;
+    private String category, company_name, role, location, job_description;
+
+    public Jobs(String category, String company_name, String role, String location, String job_description){
+        this.category = category;
+        this.company_name = company_name;
+        this.role = role;
+        this.location = location;
+        this.job_description = job_description;
+    }
+
+    public Jobs() {
+    }
 
     public String getCategory() {
         return category;
@@ -23,6 +34,27 @@ public class jobs {
     public String getJob_description() {
         return job_description;
     }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setJob_description(String job_description) {
+        this.job_description = job_description;
+    }
+
 }
 
 // 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
